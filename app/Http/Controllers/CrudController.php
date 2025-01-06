1d83ff22024819c23ac9d8b365edc081b91a2e13
VULKANNN <150655950+VULKANNN@users.noreply.github.com>
2025-01-06 16:34:08 +0700

    Add files via upload

---
+++ app/Http/Controllers/CrudController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CrudController extends Controller
+{
+    function tampil() {
+        return view('crud.tampil');
+    }
+
+    }
+
